c41d915aff0f0d7efead6d8013858aea3f3bcd2a
John Shojaei <titan.550@gmail.com>
2019-03-09 12:35:52 -0800

    Initial code to bcp pandas dataframe to sql server

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,4 +1,17 @@
 import bcpy
+import numpy as np
+import pandas as pd
+
 
 def test_basic():
     assert bcpy.name == 'bcpy'
+
+
+def test_import():
+    df = pd.DataFrame(np.random.randint(-100, 100, size=(100, 4)), columns=list('ABCD'))
+    bcpy.to_sql(df, sql_info={
+        'server': 'localhost',
+        'database': 'test_db',
+        'schema': 'dbo',
+        'table_name': 'test_table'
+    })
