a805c0779edaeafa200d98126905f7e992d76d68
John Shojaei <titan.550@gmail.com>
2019-03-16 09:08:01 -0700

    Test using docker-compose

--- tests/sqlcmd_caller.py
+++ tests/sqlcmd_caller.py
@@ -3,8 +3,8 @@ import os
 
 
 def sqlcmd_caller(command_file_path):
-    result = subprocess.run(['sqlcmd',
-                    '-S', f'mssql-server,{os.environ["TEST_MSSQL_PORT"]}',
+    result = subprocess.run(['/opt/mssql-tools/bin/sqlcmd',
+                    '-S', f'mssql,{os.environ["TEST_MSSQL_PORT"]}',
                     '-U', 'SA',
                     '-P', os.environ['TEST_MSSQL_SA_PASSWORD'],
                     '-i', command_file_path])
--- tests/test_bcp.py
+++ tests/test_bcp.py
@@ -4,8 +4,8 @@ from .sqlcmd_caller import sqlcmd_caller
 
 
 def test_create_db():
-    sqlcmd_caller('create_test_db.sql')
+    sqlcmd_caller('tests/create_test_db.sql')
 
 
 def test_create_table():
-    sqlcmd_caller('create_test_table.sql')
+    sqlcmd_caller('tests/create_test_table.sql')
