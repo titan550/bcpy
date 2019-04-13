79ee185c952b0868493eeb3c0d0c6907d3076d2a
John Shojaei <titan.550@gmail.com>
2019-04-12 19:04:29 -0700

    Removes resolved todo line

--- tests/test_bcp.py
+++ tests/test_bcp.py
@@ -1,7 +1,5 @@
 from .sqlcmd_caller import sqlcmd_caller
 
-# TODO: create docker-compose so that we can call mssql-server with dns name
-
 
 def test_create_db():
     sqlcmd_caller('tests/create_test_db.sql')
