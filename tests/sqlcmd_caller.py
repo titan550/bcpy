c6c81edfd8814507a9ff8912a3a5b48afef5e222
John Shojaei <titan.550@gmail.com>
2019-04-12 19:01:44 -0700

    Renames the environment variables and adds a wait script to delay tests to after starting mssql

--- tests/sqlcmd_caller.py
+++ tests/sqlcmd_caller.py
@@ -4,9 +4,9 @@ import os
 
 def sqlcmd_caller(command_file_path):
     result = subprocess.run(['/opt/mssql-tools/bin/sqlcmd',
-                    '-S', f'mssql,{os.environ["TEST_MSSQL_PORT"]}',
+                    '-S', f'mssql',
                     '-U', 'SA',
-                    '-P', os.environ['TEST_MSSQL_SA_PASSWORD'],
+                    '-P', os.environ['MSSQL_SA_PASSWORD'],
                     '-i', command_file_path])
     if result.returncode != 0:
         raise Exception(result.stderr) # TODO find out why stderr is empty
