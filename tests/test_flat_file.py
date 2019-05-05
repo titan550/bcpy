f5bad7a81b05b834c785df18c289fd4bdc7293cb
John Shojaei <titan.550@gmail.com>
2019-05-04 17:27:56 -0700

    Test for count and sum after loading to Sql Server

--- tests/test_flat_file.py
+++ tests/test_flat_file.py
@@ -3,12 +3,23 @@ import os
 
 
 def test_flat_file_to_sql():
-    c = bcpy.FlatFile(qualifier='', path='tests/data1.csv')
-    sql = sql = bcpy.SqlTable({
+    sql_config = {
         'server': 'mssql',
         'database': 'bcpy',
-        'table': 'test_data1',
         'username': 'SA',
+        'table': 'test',
         'password': os.environ['MSSQL_SA_PASSWORD']
-    })
-    c.to_sql(sql)
+    }
+    sql_table_name = 'test_data1'
+    csv_file_path = 'tests/data1.csv'
+    c = bcpy.FlatFile(qualifier='', path=csv_file_path)
+    sql_table = bcpy.SqlTable(sql_config, table=sql_table_name)
+    c.to_sql(sql_table)
+    sql_server = bcpy.SqlServer(sql_config)
+    count_from_sql = sql_server.run(
+        f"select count(*) from {sql_table_name}").iloc[0][0]
+    with open(csv_file_path) as f:
+        for i, _ in enumerate(f, 1):
+            pass
+    count_from_file = i - 1  # Subtracting one line because of header
+    assert count_from_file == count_from_sql
--- tests/test_pandas.py
+++ tests/test_pandas.py
@@ -1,17 +1,26 @@
-import bcpy
-import pandas as pd
-import numpy as np
 import os
+
+import numpy as np
+import pandas as pd
+
+import bcpy
 
 
 def test_import():
-    df = pd.DataFrame(np.random.randint(-100, 100, size=(100, 4)), columns=list('ABCD'))
-    bdf = bcpy.DataFrame(df)
-    sql = bcpy.SqlTable({
+    sql_config = {
         'server': 'mssql',
         'database': 'bcpy',
-        'table': 'test_dataframe',
         'username': 'SA',
         'password': os.environ['MSSQL_SA_PASSWORD']
-    })
-    bdf.to_sql(sql)
+    }
+    table_name = 'test_dataframe'
+    df = pd.DataFrame(np.random.randint(-100, 100, size=(100, 4)),
+                      columns=list('ABCD'))
+    bdf = bcpy.DataFrame(df)
+    sql_table = bcpy.SqlTable(sql_config, table=table_name)
+    bdf.to_sql(sql_table)
+    sql_server = bcpy.SqlServer(sql_config)
+    sum_of_column_d_from_sql = \
+        sql_server.run(
+            f"select sum(cast(D as int)) from {table_name}").iloc[0][0]
+    assert sum_of_column_d_from_sql == df['D'].sum()
