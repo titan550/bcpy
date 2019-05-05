68b5e71d1494f15798231ddeda59744c78184e2e
John Shojaei <titan.550@gmail.com>
2019-05-04 17:13:56 -0700

    Set sqlcmd to return a dataframe as result

--- bcpy/binary_callers.py
+++ bcpy/binary_callers.py
@@ -1,4 +1,6 @@
 import subprocess
+import pandas as pd
+from io import StringIO
 
 
 def bcp(sql_table, flat_file):
@@ -23,7 +25,26 @@ def sqlcmd(server, database, command, username=None, password=None):
         auth = ['-T']
     else:
         auth = ['-U', username, '-P', password]
-    sqlcmd_command = ['sqlcmd', '-S', '-d', database] + auth + ['-q', command]
-    result = subprocess.run(sqlcmd_command)
+    command = 'set nocount on;' + command
+    sqlcmd_command = ['sqlcmd', '-S', server, '-d', database, '-b'] + auth + \
+                     ['-s,', '-W', '-Q', command]
+    result = subprocess.run(sqlcmd_command, stdout=subprocess.PIPE)
     if result.returncode:
-        raise Exception(result.stderr)
+        print(result.stdout)
+        raise Exception(f'Sqlcmd command failed. Details:\n{result}')
+    output = StringIO(result.stdout.decode('ascii'))
+    first_line_output = output.readline().strip()
+    if first_line_output == '':
+        print(first_line_output)
+        header = None
+    else:
+        header = 'infer'
+    output.seek(0)
+    try:
+        result = pd.read_csv(
+            filepath_or_buffer=output,
+            skiprows=[1],
+            header=header)
+    except pd.errors.EmptyDataError:
+        result = None
+    return result
