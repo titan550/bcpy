1d009bf298c1ede6407cdf987cc660cb74c5f50e
John Shojaei <titan.550@gmail.com>
2019-05-04 17:07:43 -0700

    Code refactoring

--- bcpy/format_file_builder.py
+++ bcpy/format_file_builder.py
@@ -14,21 +14,29 @@ class FormatFile(object):
 
     @staticmethod
     def _scaper(input_string):
-        scaped_string = input_string.replace('"', '\\"').replace("'", "\\'").replace('\r', '\\r').replace('\n', '\\n')
+        scaped_string = input_string.replace('"', '\\"').replace("'", "\\'")\
+            .replace('\r', '\\r').replace('\n', '\\n')
         return scaped_string
 
     @classmethod
     def build_format_file(cls, flat_file):
-        format_file_row_count = len(flat_file.columns)+1 if flat_file.qualifier else len(flat_file.columns)
+        if flat_file.qualifier:
+            format_file_row_count = len(flat_file.columns) + 1
+        else:
+            format_file_row_count = len(flat_file.columns)
         format_file = f'9.0\n{format_file_row_count}\n'
         terminators = cls._get_field_terminators(flat_file)
         if flat_file.qualifier:
-            format_file += f'1 SQLCHAR 0 0 "{terminators[0]}" 0 ignored_line_start_qualifier SQL_Latin1_General_CP1_CI_AS\n'
+            format_file += f'1 SQLCHAR 0 0 "{terminators[0]}" 0 ' \
+                f'ignored_line_start_qualifier SQL_Latin1_General_CP1_CI_AS\n'
             format_file_row_index = 2
             terminators = terminators[1:]
         else:
             format_file_row_index = 1
         for column_index, terminator in enumerate(terminators, 1):
-            format_file += f'{format_file_row_index} SQLCHAR 0 0 "{terminator}" {column_index} {flat_file.columns[column_index - 1]} SQL_Latin1_General_CP1_CI_AS\n'
+            format_file += f'{format_file_row_index} SQLCHAR 0 0 ' \
+                f'"{terminator}" {column_index} ' \
+                f'{flat_file.columns[column_index - 1]} ' \
+                f'SQL_Latin1_General_CP1_CI_AS\n'
             format_file_row_index += 1
         return format_file
